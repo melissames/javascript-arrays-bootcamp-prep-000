76f39f27a0f75c116513a5340157c5fd0413377f
melissames <melissa.sames@gmail.com>
2017-12-06 18:06:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  var newArr = [element, ...array]
+  return newArr
+}
