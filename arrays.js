5638ee1e55d18bb43ea76a0f0b40a01dc1f2a84c
melissames <melissa.sames@gmail.com>
2017-12-06 18:06:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToBeginningOfArray(array, element){
   var newArr = [element, ...array]
   return newArr
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
